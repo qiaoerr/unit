22aa7c9f01ca13f5f3a0a29b73b9dab14a3cd84e
dishikun1221@sina.com <dishikun1221@sina.com@cdd530fd-73ed-c999-0f04-10abf64dd15d>
2012-09-28 00:59:23 +0000

    unit2 for web study

---
+++ trunk/Unit2Project/src/com/start/StartWeb.java
@@ -0,0 +1,27 @@
+package com.start;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class StartWeb {
+
+	public static void main(String[] args) throws IOException,
+			ServletException, LifecycleException, InterruptedException {
+
+		String currentDir = new File(".").getCanonicalPath();
+		String tomcatDir = currentDir + File.separatorChar + "tomcat";
+		String webRoot = currentDir + File.separatorChar + "WebRoot";
+
+		Tomcat tomcat = new Tomcat();
+		tomcat.setBaseDir(tomcatDir);
+		tomcat.setPort(8080);
+		tomcat.addWebapp("/unit", webRoot);
+		tomcat.start();
+		tomcat.getServer().await();
+	}
+}
